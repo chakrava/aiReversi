b9f16c56b6889d68e10f53f8434016db285c2d7c
Erik Storla <estorla@spsu.edu>
2014-11-13 19:12:42 -0500

    removing unused array

--- src/aireversi/Board.java
+++ src/aireversi/Board.java
@@ -12,8 +12,6 @@ public class Board {
     //public int[] directions = {-9, -8, -7, -1, 1, 7, 8, 9};
     public int[] directions = {-11, -10, -9, -1, 1, 9, 10, 11};
 
-    private static final int edgePositions[] = {};
-
 //  private static final int edgePositions[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 15,
 //        16, 23, 24, 31, 32, 39, 40, 47, 48, 55, 56, 57, 58, 59, 60, 61, 62, 63};
     public Board() {
