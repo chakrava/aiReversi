65cdc134380ac484ce9902c7b4d0e1fb37a27534
Erik Storla <estorla@spsu.edu>
2014-11-17 08:50:54 -0500

    cleaning up comments and unneeded includes

--- src/aireversi/AiReversi.java
+++ src/aireversi/AiReversi.java
@@ -1,8 +1,5 @@
 package aireversi;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
 /**
  *
  * @author Erik Storla
--- src/aireversi/AlphaBetaPrune.java
+++ src/aireversi/AlphaBetaPrune.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aireversi;
 
 import java.util.ArrayList;
 
 /**
  *
- * @author ckiemnstr
+ * @author Erik Storla
  */
 public class AlphaBetaPrune {
 
